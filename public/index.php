108548fad5b6827046a21237e712366e9c9c8e8f
savwicak <savwicak@gmail.com>
2024-03-13 08:57:44 +0700

    bismillah done

--- public/index.php
+++ public/index.php
@@ -16,9 +16,9 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+// if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+//     require $maintenance;
+// }
 
 /*
 |--------------------------------------------------------------------------
